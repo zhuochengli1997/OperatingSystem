38e8ef98d0b661548b5dd0bad8b07a1641afb6f9
zhuochengli1997 <lizhuocheng2016@gmail.com>
2023-02-23 14:17:44 +0100

    updated parse_input to remove quotations. exit command in command sequances will now be executed correctly

--- Lab1/shell.c
+++ Lab1/shell.c
@@ -8,25 +8,6 @@
 #include "shell.h"
 #include "scanner.h"
 
-void execute_command(char *args[]) {
-    pid_t pid = fork();
-    if (pid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (pid == 0) {
-        if (strcmp(args[0], "exit") == 0) {
-            exit(EXIT_SUCCESS);
-        }
-        if (execvp(args[0], args) == -1) {
-            perror("execvp");
-            exit(EXIT_FAILURE);
-        }
-    } else {
-        int status;
-        waitpid(pid, &status, 0);
-    }
-}
-
 bool read_input(char *input) {
     if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
         return false;
@@ -39,40 +20,25 @@ bool read_input(char *input) {
 }
 
 void execute_commands(char *input) {
-    char *command;
     char *commands[MAX_ARGS];
-    int command_count = 0;
-    int i = 0;
-
-    command = strtok(input, "&&||;");
-    while (command != NULL && command_count < MAX_ARGS) {
-        commands[command_count++] = command;
-        command = strtok(NULL, "&&||;");
-    }
+    int command_count = split_commands(input, commands);
 
     int status = 0;
-    for (i = 0; i < command_count; i++) {
+    for (int i = 0; i < command_count; i++) {
         char *args[MAX_ARGS];
-        int arg_count = 0;
-        char *arg = strtok(commands[i], " ");
-        while (arg != NULL && arg_count < MAX_ARGS) {
-            args[arg_count++] = arg;
-            arg = strtok(NULL, " ");
-        }
+        int arg_count = split_args(commands[i], args);
         args[arg_count] = NULL;
+
+        if (strcmp(args[0], "exit") == 0) {
+            exit(EXIT_SUCCESS);
+        }
 
         pid_t pid = fork();
         if (pid == -1) {
             perror("fork");
             exit(EXIT_FAILURE);
         } else if (pid == 0) {
-            if (strcmp(args[0], "exit") == 0) {
-                exit(EXIT_SUCCESS);
-            }
-            if (execvp(args[0], args) == -1) {
-                perror("execvp");
-                exit(EXIT_FAILURE);
-            }
+            execute_command(args);
         } else {
             waitpid(pid, &status, 0);
             if (status != 0) {
@@ -82,7 +48,48 @@ void execute_commands(char *input) {
     }
 }
 
+int split_commands(char *input, char *commands[]) {
+    int command_count = 0;
+    char *command = strtok(input, "&&||;");
+    while (command != NULL && command_count < MAX_ARGS) {
+        commands[command_count++] = command;
+        command = strtok(NULL, "&&||;");
+    }
+    return command_count;
+}
+
+int split_args(char *command, char *args[]) {
+    int arg_count = 0;
+    char *arg = strtok(command, " ");
+    while (arg != NULL && arg_count < MAX_ARGS) {
+        args[arg_count++] = arg;
+        arg = strtok(NULL, " ");
+    }
+    return arg_count;
+}
+
+void execute_command(char *args[]) {
+    if (strcmp(args[0], "exit") == 0) {
+        exit(EXIT_SUCCESS);
+    }
+    if (execvp(args[0], args) == -1) {
+        perror("execvp");
+        exit(EXIT_FAILURE);
+    }
+}
+
+
 void parse_input(char *input) {
+    char* p = input;
+    char* q = input;
+    while (*q != '\0') {
+        if (*q != '\"') {
+            *p = *q;
+            p++;
+        }
+        q++;
+    }
+    *p = '\0';
     execute_commands(input);
 }
 
--- Lab1/shell.h
+++ Lab1/shell.h
@@ -8,8 +8,14 @@
 
 void execute_command(char *args[]);
 
+void execute_commands(char *input);
+
 bool read_input(char *input);
 
 void parse_input(char *input);
+
+int split_commands(char *input, char *commands[]);
+
+int split_args(char *command, char *args[]);
 
 #endif /* SHELL_H */
